ab717b9963637dc3ade8146505ef99209872c098
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-09-28 10:38:20 +0800

    redis队列

---
+++ src/main/java/com/bonaparte/service/RedisQueueService.java
@@ -0,0 +1,20 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisCommands;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by yangmingquan on 2018/9/28.
+ */
+@Service
+public class RedisQueueService {
+    @Resource(name = "jedisCluster")
+    private JedisCommands jedis;
+
+    public void queueSenior(){
+        jedis.lpush("listBonaparte", "abc11111111111");
+        jedis.blpop(10, "listBonaparte");
+    }
+}
